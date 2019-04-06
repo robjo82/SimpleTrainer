322e7b6cc9fc98cddae2ae3ab1aed6ea12594184
robjo82 <rjrdutg@gmail.com>
2019-04-06 09:05:59 +0200

    Modification automatique des pycaches, modification du fichier views.py de EspacePerso. Au moment ou j'écris ces lignes, le projet se lance après avoir été importé de github, l'erreur à corriger est l'acces ç l'Espace Perso en connexion sociale.

--- EspacePerso/views.py
+++ EspacePerso/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_objectd_or_404
+from django.shortcuts import render
 from bibliotheque.models import Article
 from . import models
 from .models import Profil
@@ -40,5 +40,5 @@ def connect (request) :
 
 @login_required
 def personal_space (request):
-    profil = Profil.objects.get_objectd_or_404(user_id = request.user.id)
+    profil = Profil.objects.get(user_id = request.user.id)
     return render (request, 'EspacePerso/personal_space.html', {'last_five' : last_five, "profil" : profil})
